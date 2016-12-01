0fc25e6ab12229338fbd3d0801741d1452aa0629
Garrett Anderson <j.garrett.anderson@gmail.com>
2016-12-01 03:07:22 -0800

    Indentation fixes

--- mainapp/auth.py
+++ mainapp/auth.py
@@ -14,10 +14,29 @@ APPLICATION_NAME = 'GFunk\'s Mail Merger'
 
 def get_credentials():
     """ Obtains user's OAuth credentials """
+    if 'credentials' not in flask.session:
+        return flask.redirect(flask.url_for('oauth2callback'))
+        credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+    if credentials.access_token_expired:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    else:
+        http_auth = credentials.authorize(httplib2.Http())
 
+
+def oauth2_call():
     flow = client.flow_from_clientsecrets(
         CLIENT_SECRET_FILE,
         scope=SCOPES,
-        redirect_uri='http://localhost:8081/')
+        redirect_uri=flask.url_for('oauth2callback', _external=True),
+        include_granted_scopes=True)
+
+    if 'code' not in flask.request.args:
+        auth_uri = flow.step1_get_authorize_url()
+        return flask.redirect(auth_uri)
+    else:
+        auth_code = flask.request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
+        flask.session['credentials'] = credentials.to_json()
+
 
 
