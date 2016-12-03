d899da51d64c01f5edcd8b7a63a6afe5d70c3712
Garrett Anderson <j.garrett.anderson@gmail.com>
2016-12-03 13:34:31 -0800

    Not sure

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,13 +13,23 @@ from mainapp.auth import get_credentials
 
 @app.route("/")
 def index():
-    get_credentials()
+    if 'credentials' in flask.session:
+        return flask.redirect(flask.url_for('mail_merge'))
     return app.send_static_file("index.html")
+
+@app.route("/auth")
+def auth():
+    get_credentials()
 
 @app.route("/auth")
 def oauth2callback():
     oauth2_call()
-    return flask.redirect(flask.url_for('index'))
+    return flask.redirect(flask.url_for('mail_merge'))
+
+@app.route("/mailmerge")
+def mail_merge():
+    if 'credentials' not in flask.session:
+        return flask.redirect(flask.url_for('index'))
 
 if __name__ == '__main__':
     import uuid
